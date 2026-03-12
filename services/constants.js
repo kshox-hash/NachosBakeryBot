91aaf4040f5dc18e99f05c705938db7e84e7d3a3
kshox❤️ <josepenacurihual@gmail.com>
2026-03-12 12:47:58 -0300

    first commit

---
+++ services/constants.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2021-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+"use strict";
+
+module.exports = Object.freeze({
+  // Mensajes esperados del usuario
+  USER_START_MESSAGE: "Toca enviar para comenzar",
+
+  // Mensajes de respuesta
+  APP_DEFAULT_MESSAGE: "¡Bienvenido a Chumingo's Bakery! ¿En qué podemos ayudarte hoy?",
+  APP_TRY_ANOTHER_MESSAGE: "¿Hay algo más en lo que podamos ayudarte?",
+
+  // Textos de botones (CTA)
+  REPLY_INTERACTIVE_WITH_MEDIA_CTA: "Comprar en línea",
+  REPLY_MEDIA_CARD_CAROUSEL_CTA: "Ver ideas de recetas",
+  REPLY_OFFER_CTA: "Promoción actual",
+
+  // IDs de botones de respuesta
+  REPLY_INTERACTIVE_MEDIA_ID: "reply-interactive-with-media",
+  REPLY_MEDIA_CAROUSEL_ID: "reply-media-card-carousel",
+  REPLY_OFFER_ID: "reply-offer"
+});
